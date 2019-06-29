fd086cc7771a9d6c27f995662fa6cf80ba1a8fa0
CuriousEngineer <kirill.s.works@gmail.com>
2019-06-29 21:21:08 +0300

    Added all interfaces and parent class

--- src/com/company/Set.java
+++ src/com/company/Set.java
@@ -1,4 +1,17 @@
 package com.company;
 
-public class Set {
+import java.io.Serializable;
+import java.util.AbstractSet;
+import java.util.Iterator;
+
+public class Set<E> extends AbstractSet implements Serializable, Cloneable {
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
